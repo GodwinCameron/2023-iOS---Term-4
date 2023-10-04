42199e3d8cbae92117989ccfc414da8b42dd5da0
Godwin Cameron <200109@virtualwindow.co.za>
2023-10-04 03:48:33 +0200

    Initial Commit

---
+++ 2023 iOS - Term 4/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  2023 iOS - Term 4
+//
+//  Created by Shiro NoKage on 2023/10/04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
